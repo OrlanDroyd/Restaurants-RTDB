5dfbac4cf46f1c5ff040218ab159dba553fc557b
OrlanDroyd <orlandroyd@gmail.com>
2023-07-29 00:21:18 -0400

    fix launcher MainActivity

--- app/src/main/java/com/gmail/orlandroyd/restaurants_rtdb/restaurants/presentation/MainActivity.kt
+++ app/src/main/java/com/gmail/orlandroyd/restaurants_rtdb/restaurants/presentation/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.gmail.orlandroyd.restaurants_rtdb.restaurants.presentation
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -9,7 +12,20 @@ import androidx.navigation.navArgument
 import androidx.navigation.navDeepLink
 import com.gmail.orlandroyd.restaurants_rtdb.restaurants.presentation.details.RestaurantDetailsScreen
 import com.gmail.orlandroyd.restaurants_rtdb.restaurants.presentation.list.RestaurantsScreen
+import com.gmail.orlandroyd.restaurants_rtdb.ui.theme.RestaurantsRTDBTheme
 
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RestaurantsRTDBTheme {
+                RestaurantsApp()
+            }
+        }
+    }
+}
 
 @Composable
 private fun RestaurantsApp() {
